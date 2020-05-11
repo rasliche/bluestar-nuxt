88081fb696a5102937a7f9da5db8f1989ea8e5e4
Eric Raslich <ericraslich@MacBook-Pro.local>
2020-05-11 16:22:38 -0400

    Add pledge

---
+++ pages/pledge.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Thank you for booking your trip with a Blue Star operator</h1>
+    <ol>
+      <li>Obey the regulations</li>
+    </ol>
+    <input type="text" />Sign your name here
+    <p>Sign the Pledge</p>
+  </div>
+</template>
--- pages/training/lessons/index.vue
+++ pages/training/lessons/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
+  <div class="w-full">
     <h1>Lessons Index</h1>
-    <div class="flex flex-wrap">
+    <div class="flex flex-wrap space-around">
       <nuxt-link to="/training/lessons/about-blue-star">
         About Blue Star
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/sanctuary-regulations-and-zones">
+        Sanctuary Regulations and Zones
       </nuxt-link>
     </div>
   </div>
