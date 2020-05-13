ed50c4cc83a13f0bbaa6abaa3c4f3acad98536cc
Eric Raslich <ericraslich@MacBook-Pro.local>
2020-05-13 12:12:04 -0400

    Integrate Story Map into regs and zones lesson

--- pages/training/lessons/sanctuary-regulations-and-zones.vue
+++ pages/training/lessons/sanctuary-regulations-and-zones.vue
@@ -7,96 +7,15 @@
       that these activities only happen in ways - and at places - that are
       compatible with conserving sanctuary resources.
     </p>
-    <img src="" alt="" />
 
-    <h2 class="text-xl font-semibold">Why Blue Star?</h2>
-    <p>
-      Florida Keys coral reefs are a main draw for your customers. The reefs of
-      the Florida Keys are stressed. And when reef users are uninformed or
-      irresponsible, they can, and often do, make these stresses even worse by
-      damaging coral. That’s bad news.
-    </p>
-    <p>
-      The good news is that, as a Blue Star partner, you have the opportunity to
-      teach visitors about the reef and how to safely interact with it. The
-      information that you provide could change visitors’ behaviors to be
-      reef-aware.
-    </p>
-
-    <!-- <text-on-scroll>The Briefing</text-on-scroll> -->
-    <h2 class="text-xl font-semibold">The Heart of Blue Star - The Briefing</h2>
-    <p>
-      From 2010 to 2012 two separate research studies were conducted in the
-      Florida Keys to determine how a pre-dive education briefing can influence
-      diver impacts on coral reefs. Research has shown that divers on NOAA Blue
-      Star Certified charters “cause significantly less coral damage than do
-      divers from other types of dive shops” (Kreiger & Chadwick, 2012). Dive
-      briefings administered by Blue Star Operators are likely the contributing
-      factor which limits diver contact with the reefs.
-    </p>
-    <p>
-      On average, recreational SCUBA divers come in contact with live coral
-      eighteen times per dive (Kreiger & Chadwick, 2012). Many touches are
-      caused by fin kicks or loose equipment dragging on the reef (Camp &
-      Fraser, 2012). The incorporation of conservation education into onboard
-      briefings improves customer understanding of environmental threats, and
-      gives them a last minute reminder to be cautious of their behavior while
-      underwater, limiting coral contact. According to Krieger and Chadwick
-      (2012), customers who received dive briefings that meet Blue Star
-      requirements prior to entering the water caused considerably less reef
-      damage than customers who did not.
-    </p>
-    <p>
-      It is estimated that due to increasingly high rates of recreational diving
-      on sites in Key Largo alone, over 80% of live coral colonies have been
-      damaged and some heavily dived sites have seen a reduction in live coral
-      to less than 11% of the total cover. Given that the proportion of damaged
-      corals on each reef increases tremendously with estimated rates of
-      recreational SCUBA diving on those sites, diver education with the goal of
-      reducing reef contact is essential to ensuring tourism rates are both
-      economically and environmentally sustainable. The effort of Blue Star
-      Operators to train their staff and promote environmental stewardship is a
-      monumental success in the conservation of the Florida Keys National Marine
-      Sanctuary.
-    </p>
-    <p>
-      The Blue Star program is one way that you can show customers – people who
-      were drawn to the area because of the environment in the first place –
-      that you, and the business that you work for, care about the ecosystem and
-      are committed to doing your part to protect it. Please take the time to
-      memorize each component of the Blue Star briefing. You can incorporate
-      these components into your current briefing. Feel free to add your own
-      style while making sure you include each required section. You are
-      required to give the Blue Star briefing on every trip that you take.
-    </p>
-
-    <h2 class="text-xl font-semibold">
-      Blue Star Requirements - Understanding the Framework
-    </h2>
-    <p>
-      Blue Star operators have made a commitment to train their staff and
-      customers to be better environmental stewards. As part of the program,
-      staff who work at Blue Star recognized dive shops are required to complete
-      this online training each year on issues affecting coral reefs, sanctuary
-      regulations and coral reef etiquette. In turn, staff must educate their
-      customers on these topics. This includes showing students a Blue Star
-      video during training classes and conducting a Blue Star dive briefing on
-      each trip that you take. Blue Star Diving Operators must offer
-      conservation-related dive courses and participate in activities such as
-      fish counts and reef clean-ups.
-    </p>
-    <p>
-      Once a business has gone through the initial evaluation and is recognized
-      as a Blue Star partner, they will be reevaluated each year, to ensure that
-      the Blue Star criteria are still being followed.
-    </p>
-    <p>
-      You should have a copy of the current years Blue Star Diving framework,
-      which outlines the specifics of what each operator is required to do to be
-      “Blue Star recognized.” As a staff member at a Blue Star operation, we are
-      depending on you to be familiar with this framework and properly implement
-      the Blue Star Diving program.
-    </p>
+    <iframe
+      width="100%"
+      height="800px"
+      src="https://noaa.maps.arcgis.com/apps/MapJournal/index.html?appid=5d5868f80cdb4b4c80e54ac47a0bf069"
+      frameborder="0"
+      scrolling="no"
+    >
+    </iframe>
   </div>
 </template>
 
