65a4034c82cb0a412eb4350a77b4db90e1a3398a
Eric Raslich <ericraslich@MacBook-Pro.local>
2020-05-13 11:25:43 -0400

    Remove boilerplate links to documentation.

--- pages/index.vue
+++ pages/index.vue
@@ -7,21 +7,6 @@
         For Those Who Go Beyond
         <span class="text-blue-700">Best Practices</span>
       </h1>
-      <h2 class="subtitle">
-        My super-duper Nuxt.js project
-      </h2>
-      <div class="flex flex-wrap mx-auto">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
