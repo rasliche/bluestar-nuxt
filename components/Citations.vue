8c832b5132edf5c570e6767b934c7dba74e0ebd0
Eric Raslich <ericraslich@MacBook-Pro.local>
2020-05-17 15:59:46 -0400

    Generalize Citations to use for citations and crediting

---
+++ components/Citations.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <span class="inline-flex" @click="toggleOpen">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        :class="{ '-rotate-90': !open }"
+        class="w-6 icon-cheveron-down transform transition-transform duration-500"
+      >
+        <path
+          class="text-current"
+          fill-rule="evenodd"
+          d="M15.3 10.3a1 1 0 0 1 1.4 1.4l-4 4a1 1 0 0 1-1.4 0l-4-4a1 1 0 0 1 1.4-1.4l3.3 3.29 3.3-3.3z"
+        ></path>
+      </svg>
+      <p>
+        {{ summary }}
+      </p>
+    </span>
+    <transition name="drawer">
+      <ul v-show="open" class="list-disc list-outside space-y-3">
+        <slot></slot>
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Citations',
+  props: {
+    summary: {
+      type: String,
+      default: 'Details'
+    }
+  },
+  data() {
+    return {
+      open: true
+    }
+  },
+  methods: {
+    toggleOpen() {
+      this.open = !this.open
+    }
+  }
+}
+</script>
+
+<style>
+.drawer-enter-active,
+.drawer-leave-active {
+  transition: all 0.5s ease;
+}
+
+.drawer-enter,
+.drawer-leave-to {
+  opacity: 0;
+  /* transform: translateY(-50px); */
+}
+
+.drawer-enter-to,
+.drawer-leave {
+  opacity: 1;
+}
+</style>
---
+++ components/CitationsSource.vue
@@ -0,0 +1,17 @@
+<template>
+  <li :id="anchor" class="ml-4">
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'CitationsSource',
+  props: {
+    anchor: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
