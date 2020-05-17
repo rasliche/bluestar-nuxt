08a957ef8d6779b8449909718691a073366a3bc3
Eric Raslich <ericraslich@MacBook-Pro.local>
2020-05-17 16:00:01 -0400

    Admin Dashboard

---
+++ pages/admin/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Admin',
+  async asyncData({ app }) {
+    const message = await app.$axios.$get('/api/users')
+    return { message }
+  },
+  data() {
+    return {
+      message: 'default'
+    }
+  },
+  head() {
+    return [
+      {
+        title: 'Admin Dashboard'
+      }
+    ]
+  }
+}
+</script>
