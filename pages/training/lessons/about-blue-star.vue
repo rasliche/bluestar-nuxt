33441b5af714095570c39de03cc077e62e06a84a
Eric Raslich <ericraslich@MacBook-Pro.local>
2020-05-17 16:02:39 -0400

    Add and use Citation components

--- pages/training/lessons/about-blue-star.vue
+++ pages/training/lessons/about-blue-star.vue
@@ -1,48 +1,61 @@
 <template>
   <div>
-    <h1 class="max-w-6xl my-8 text-4xl border-b-2 border-blue-800">
+    <h1
+      class="max-w-3xl my-8 text-6xl border-b-2 text-blue-800 border-blue-800"
+    >
       About Blue Star
     </h1>
-    <h2 class="max-w-6xl my-4 text-2xl font-semibold">What is Blue Star?</h2>
-    <p class="max-w-6xl my-2 italic">
+    <h2 class="max-w-3xl mt-4 mb-1 text-2xl text-blue-800 font-semibold">
+      What is Blue Star?
+    </h2>
+    <p class="max-w-3xl leading-relaxed my-2">
       The Blue Star program, established by the Florida Keys National Marine
       Sanctuary, is a voluntary recognition program saluting charter boat
       operators who promote responsible and sustainable diving, snorkeling and
       fishing practices, helping keep Florida Keys ecosystems healthy for
       generations to come.
     </p>
-    <p class="max-w-6xl my-2 italic">
+    <p class="max-w-3xl leading-relaxed my-2">
       As a Blue Star partner, your business has voluntarily agreed to educate
       customers about the coral reef ecosystem, the sanctuary, diving and
       snorkeling etiquette, and/or sustainable fishing best practices. By
       passing this information on to visitors who recreate in the Florida Keys,
       you are playing an important role in helping to protect our reefs.
     </p>
-    <div class="max-w-6xl rounded-md overflow-hidden">
+
+    <div class="max-w-3xl my-8 rounded-md overflow-hidden">
       <img
         src="https://res.cloudinary.com/duzmgsio4/image/upload/f_auto,q_auto:eco/v1583011565/fknms-bluestar/dive-boat-mate-briefs-guests.jpg"
       />
+      <p class="text-sm">
+        A Blue Star guide at Keys Diver briefs guests on best practices for
+        snorkeling and protecting coral reefs.
+        <span class="font-semibold">
+          Photo Credit: FKNMS/Marlies Tumolo
+        </span>
+      </p>
     </div>
 
-    <h2 class="max-w-6xl my-4 text-2xl font-semibold">Why Blue Star?</h2>
-    <p class="max-w-6xl my-2">
+    <h2 class="max-w-3xl mt-4 mb-1 text-2xl text-blue-800 font-semibold">
+      Why Blue Star?
+    </h2>
+    <p class="max-w-3xl leading-relaxed my-2">
       Florida Keys coral reefs are a main draw for your customers. The reefs of
       the Florida Keys are stressed. And when reef users are uninformed or
       irresponsible, they can, and often do, make these stresses even worse by
       damaging coral. That’s bad news.
     </p>
-    <p class="max-w-6xl my-2">
+    <p class="max-w-3xl leading-relaxed my-2">
       The good news is that, as a Blue Star partner, you have the opportunity to
       teach visitors about the reef and how to safely interact with it. The
       information that you provide could change visitors’ behaviors to be
       reef-aware.
     </p>
 
-    <!-- <text-on-scroll>The Briefing</text-on-scroll> -->
-    <h2 class="max-w-6xl my-4 text-2xl font-semibold">
+    <h2 class="max-w-3xl mt-4 mb-1 text-2xl text-blue-800 font-semibold">
       The Heart of Blue Star - The Briefing
     </h2>
-    <p class="max-w-6xl my-2">
+    <p class="max-w-3xl leading-relaxed my-2">
       From 2010 to 2012 two separate research studies were conducted in the
       Florida Keys to determine how a pre-dive education briefing can influence
       diver impacts on coral reefs. Research has shown that divers on NOAA Blue
@@ -57,29 +70,38 @@
       . Dive briefings administered by Blue Star Operators are likely the
       contributing factor which limits diver contact with the reefs.
     </p>
-    <p class="max-w-6xl my-2">
+    <p class="max-w-3xl leading-relaxed my-2">
       On average, recreational SCUBA divers come in contact with live coral
       eighteen times per dive
-      <a href="#kc2012">
+      <a
+        href="#kc2012"
+        class="border-b-2 border-blue-500 transition-colors duration-500 hover:bg-blue-500"
+      >
         (Kreiger & Chadwick, 2012)
       </a>
       . Many touches are caused by fin kicks or loose equipment dragging on the
       reef
-      <a href="#cf2012">
+      <a
+        href="#cf2012"
+        class="border-b-2 border-blue-500 transition-colors duration-500 hover:bg-blue-500"
+      >
         (Camp & Fraser, 2012)
       </a>
       . The incorporation of conservation education into onboard briefings
       improves customer understanding of environmental threats, and gives them a
       last minute reminder to be cautious of their behavior while underwater,
       limiting coral contact. According to
-      <a href="#kc2012">
+      <a
+        href="#kc2012"
+        class="border-b-2 border-blue-500 transition-colors duration-500 hover:bg-blue-500"
+      >
         (Kreiger & Chadwick, 2012)
       </a>
       , customers who received dive briefings that meet Blue Star requirements
       prior to entering the water caused considerably less reef damage than
       customers who did not.
     </p>
-    <p class="max-w-6xl my-2">
+    <p class="max-w-3xl leading-relaxed my-2">
       It is estimated that due to increasingly high rates of recreational diving
       on sites in Key Largo alone, over 80% of live coral colonies have been
       damaged and some heavily dived sites have seen a reduction in live coral
@@ -92,7 +114,7 @@
       monumental success in the conservation of the Florida Keys National Marine
       Sanctuary.
     </p>
-    <p class="max-w-6xl my-2">
+    <p class="max-w-3xl leading-relaxed my-2">
       The Blue Star program is one way that you can show customers – people who
       were drawn to the area because of the environment in the first place –
       that you, and the business that you work for, care about the ecosystem and
@@ -103,10 +125,10 @@
       required to give the Blue Star briefing on every trip that you take.
     </p>
 
-    <h2 class="max-w-6xl my-4 text-2xl font-semibold">
+    <h2 class="max-w-3xl mt-4 mb-1 text-2xl text-blue-800 font-semibold">
       Blue Star Requirements - Understanding the Framework
     </h2>
-    <p class="max-w-6xl my-2">
+    <p class="max-w-3xl leading-relaxed my-2">
       Blue Star operators have made a commitment to train their staff and
       customers to be better environmental stewards. As part of the program,
       staff who work at Blue Star recognized dive shops are required to complete
@@ -118,43 +140,43 @@
       conservation-related dive courses and participate in activities such as
       fish counts and reef clean-ups.
     </p>
-    <p class="max-w-6xl my-2">
+    <p class="max-w-3xl leading-relaxed my-2">
       Once a business has gone through the initial evaluation and is recognized
       as a Blue Star partner, they will be reevaluated each year, to ensure that
       the Blue Star criteria are still being followed.
     </p>
-    <p class="max-w-6xl my-2">
+    <p class="max-w-3xl leading-relaxed my-2">
       You should have a copy of the current year Blue Star Diving framework,
       which outlines the specifics of what each operator is required to do to be
       “Blue Star recognized.” As a staff member at a Blue Star operation, we are
       depending on you to be familiar with this framework and properly implement
       the Blue Star Diving program.
     </p>
-    <details class="mx-auto" open>
-      <summary>Citations</summary>
-      <ul class="list-disc list-inside space-y-3">
-        <li id="kc2012">
-          Recreational diving impacts and the use of pre-dive briefings as a
-          management strategy on Florida coral reefs (Joseph R. Krieger and
-          Nanette E. Chadwick)
-        </li>
-        <li id="cf2012">
-          Influence of conservation education dive briefings as a management
-          tool on the timing and nature of recreational SCUBA diving impacts on
-          coral reefs (Emma Camp and Douglas Fraser)
-        </li>
-      </ul>
-    </details>
+
+    <Citations :summary="'Citations'" class="max-w-3xl mt-8">
+      <CitationsSource :anchor="'kc2012'">
+        Recreational diving impacts and the use of pre-dive briefings as a
+        management strategy on Florida coral reefs (Joseph R. Krieger and
+        Nanette E. Chadwick)
+      </CitationsSource>
+      <CitationsSource :anchor="'cf2012'">
+        Influence of conservation education dive briefings as a management tool
+        on the timing and nature of recreational SCUBA diving impacts on coral
+        reefs (Emma Camp and Douglas Fraser)
+      </CitationsSource>
+    </Citations>
   </div>
 </template>
 
 <script>
-// import TextOnScroll from '~/components/TextOnScroll.vue'
+import Citations from '~/components/Citations.vue'
+import CitationsSource from '~/components/CitationsSource.vue'
 
 export default {
   name: 'AboutBlueStar',
   components: {
-    // TextOnScroll
+    Citations,
+    CitationsSource
   },
   computed: {
     passOrFail() {
