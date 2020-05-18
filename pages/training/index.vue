97f0d51603b103fda707df4d1ac634836dfb1dec
Eric Raslich <ericraslich@MacBook-Pro.local>
2020-05-18 12:07:46 -0400

    Training Index page

--- pages/training/index.vue
+++ pages/training/index.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
-    <h1>Training Index</h1>
-    <div class="flex flex-wrap">
-      <nuxt-link to="/training/lessons">All lessons</nuxt-link>
+    <h1
+      class="max-w-3xl my-8 text-6xl border-b-2 text-blue-800 border-blue-800"
+    >
+      Choose a Learning Path
+    </h1>
+    <div class="flex flex-wrap space-x-2">
+      <div class="w-1/4 h-64 bg-green-200">
+        Diving
+      </div>
+      <div class="w-1/4 h-64 bg-green-200">
+        Fishing
+      </div>
+      <div class="w-1/4 h-64 bg-green-200">
+        Volunteering
+      </div>
     </div>
+    <nuxt-link to="/training/lessons">All lessons</nuxt-link>
   </div>
 </template>
 
@@ -19,5 +32,3 @@ export default {
   }
 }
 </script>
-
-<style></style>
