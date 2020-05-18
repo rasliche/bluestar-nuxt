65500689942383beae9ab345c7583be2058c2a61
Eric Raslich <ericraslich@MacBook-Pro.local>
2020-05-18 14:29:22 -0400

    Me dashboard

---
+++ pages/users/me.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>My Dashboard</h1>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  asyncData() {
+    const message = 'reached your user dashboard page'
+    return { message }
+  },
+  data() {
+    return {
+      message: 'default'
+    }
+  },
+  head() {
+    return [
+      {
+        title: 'Dashboard'
+      }
+    ]
+  }
+}
+</script>
