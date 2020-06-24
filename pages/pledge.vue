0ac9e74514a6213c671671c188f5cf7cc6494091
Eric Raslich <rasliche@gmail.com>
2020-06-23 20:11:04 -0400

    Pledge items

--- pages/pledge.vue
+++ pages/pledge.vue
@@ -3,6 +3,8 @@
     <h1>Thank you for booking your trip with a Blue Star operator</h1>
     <ol>
       <li>Obey the regulations</li>
+      <li>Leave only bubbles</li>
+      <li>Obey the regulations</li>
     </ol>
     <input type="text" />Sign your name here
     <p>Sign the Pledge</p>
