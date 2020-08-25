b9649010ca8c217a6d0d69d1f252d08be0cfbb5d
Eric Raslich <rasliche@gmail.com>
2020-08-25 13:15:52 -0400

    feat: Topic pages list all training modules

--- pages/training/diving.vue
+++ pages/training/diving.vue
@@ -3,18 +3,44 @@
     <PageHeading>
       Diving Topics
     </PageHeading>
-    <div class="flex flex-wrap space-x-2">
-      <div class="w-1/4 h-64 bg-green-200">
-        Diving
-      </div>
-      <div class="w-1/4 h-64 bg-green-200">
-        Fishing
-      </div>
-      <div class="w-1/4 h-64 bg-green-200">
-        Volunteering
-      </div>
+    <div class="flex flex-wrap w-full justify-around">
+      <nuxt-link to="/training/lessons/about-blue-star">
+        About Blue Star
+      </nuxt-link>
+      <nuxt-link
+        to="/training/lessons/about-florida-keys-national-marine-sanctuary"
+      >
+        About Florida Keys National Marine Sanctuary
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/coral-reefs">
+        Coral Reefs
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/coral-disease">
+        Coral Disease
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/coral-threats">
+        Coral Threats
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/cultural-resources">
+        Cultural Resources
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/lionfish">
+        Lionfish
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/mangroves-and-seagrass">
+        Mangroves and Seagrass
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/marine-debris">
+        Marine Debris
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/reef-etiquette">
+        Reef Etiquette
+      </nuxt-link>
+      <nuxt-link to="/training/lessons/sanctuary-regulations-and-zones">
+        Sanctuary Regulations and Zones
+      </nuxt-link>
     </div>
-    <nuxt-link to="/training/lessons">All lessons</nuxt-link>
+    <nuxt-link to="/training">Back to Training</nuxt-link>
   </div>
 </template>
 
