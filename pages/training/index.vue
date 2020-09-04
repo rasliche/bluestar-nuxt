cdd78b005d7d881c26ee89ed45be348da961fc13
Eric Raslich <rasliche@gmail.com>
2020-09-04 11:18:00 -0400

    fix: style links in training index pages

--- pages/training/index.vue
+++ pages/training/index.vue
@@ -3,7 +3,7 @@
     <PageHeading>
       Choose a Learning Path
     </PageHeading>
-    <div class="flex flex-wrap space-x-2">
+    <div class="flex space-x-2">
       <nuxt-link to="/training/diving" class="w-1/4 h-64 bg-green-200">
         Diving
       </nuxt-link>
@@ -14,40 +14,71 @@
         Volunteering
       </nuxt-link>
     </div>
-    <div class="flex flex-wrap w-full justify-around">
-      <nuxt-link to="/training/lessons/about-blue-star">
+    <div class="flex flex-wrap">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/about-blue-star"
+      >
         About Blue Star
       </nuxt-link>
       <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
         to="/training/lessons/about-florida-keys-national-marine-sanctuary"
       >
         About Florida Keys National Marine Sanctuary
       </nuxt-link>
-      <nuxt-link to="/training/lessons/coral-reefs">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/coral-reefs"
+      >
         Coral Reefs
       </nuxt-link>
-      <nuxt-link to="/training/lessons/coral-disease">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/coral-disease"
+      >
         Coral Disease
       </nuxt-link>
-      <nuxt-link to="/training/lessons/coral-threats">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/coral-threats"
+      >
         Coral Threats
       </nuxt-link>
-      <nuxt-link to="/training/lessons/cultural-resources">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/cultural-resources"
+      >
         Cultural Resources
       </nuxt-link>
-      <nuxt-link to="/training/lessons/lionfish">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/lionfish"
+      >
         Lionfish
       </nuxt-link>
-      <nuxt-link to="/training/lessons/mangroves-and-seagrass">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/mangroves-and-seagrass"
+      >
         Mangroves and Seagrass
       </nuxt-link>
-      <nuxt-link to="/training/lessons/marine-debris">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/marine-debris"
+      >
         Marine Debris
       </nuxt-link>
-      <nuxt-link to="/training/lessons/reef-etiquette">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/reef-etiquette"
+      >
         Reef Etiquette
       </nuxt-link>
-      <nuxt-link to="/training/lessons/sanctuary-regulations-and-zones">
+      <nuxt-link
+        class="border-black border-2 py-3 px-4 m-4 font-semibold transition-colors ease-in-out duration-300 bg-blue-200 text-blue-900 hover:bg-black hover:text-blue-100"
+        to="/training/lessons/sanctuary-regulations-and-zones"
+      >
         Sanctuary Regulations and Zones
       </nuxt-link>
     </div>
