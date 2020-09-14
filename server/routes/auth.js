8a51034d4999078ad204f9a42a919d1714401d7c
Jason Ball <jasonericball@gmail.com>
2020-09-14 11:55:54 -0400

    Switch from bcrypt to bcryptjs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const User = require('../models/User')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
 
 router.post('/login',
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
 const User = require('../models/User')
 const authenticated = require('../middleware/authenticated')
