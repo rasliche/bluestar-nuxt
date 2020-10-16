0260829233261433f77583d709134e66aef74fb7
Eric Raslich <rasliche@gmail.com>
2020-10-16 13:02:35 -0400

    Pretty format

--- pages/users/me.vue
+++ pages/users/me.vue
@@ -2,8 +2,18 @@
   <div>
     <PageHeading>My Dashboard</PageHeading>
     <div class="flex w-full text-center divide-x divide-blue-300">
-      <p class="flex-1 text-lg">{{ this.$auth.user.name }}</p>
-      <p class="flex-1 text-lg">{{ this.$auth.user.email }}</p>
+      <div class="flex-1 text-lg">
+        <p>{{ this.$auth.user.name }}</p>
+      </div>
+      <div class="flex-1 text-lg">
+        <p>{{ this.$auth.user.email }}</p>
+      </div>
+    </div>
+    <div>
+      <h2>Completed Lessons</h2>
+      <ul>
+        <li></li>
+      </ul>
     </div>
     {{ this.$auth.user }}
   </div>
