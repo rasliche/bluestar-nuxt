4380c1a543649f23fc980d1fa501c604d78cd83a
Eric Raslich <rasliche@gmail.com>
2020-10-21 18:49:08 -0400

    Check admin password upon user registration

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -38,7 +38,7 @@ router.post('/admin',[
     .exists()
     .custom((value, { req }) => value === req.body.password),
   check('adminPassword')
-    .exists()
+    .equals(process.env.BS_ADMIN_PASSWORD)
     // TODO pluck an admin password from env and check for it here
   ], 
   async (req, res, next) => {
