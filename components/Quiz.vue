6d64e8eb90b11e91c3105076c386a66be7549226
Jason Ball <jasonericball@gmail.com>
2020-11-01 15:41:13 -0500

    Dynamically load questions

--- components/Quiz.vue
+++ components/Quiz.vue
@@ -2,22 +2,83 @@
   <div
     class="quiz border border-blue-800 flex flex-col rounded-t-md rounded-b-md"
   >
-    <p class="quiz-header">Quiz (0/1)</p>
+    <p class="quiz-header">
+      Quiz ({{ questionIndex + 1 }}/{{ questionCount }})
+    </p>
     <p class="quiz-question block text-center">
-      Which of these is NOT a programming language?
+      {{ questions[questionIndex].question }}
     </p>
     <hr />
-    <button class="quiz-answer-choice block text-center btn">JavaScript</button>
-    <button class="quiz-answer-choice block text-center btn">C++</button>
-    <button class="quiz-answer-choice block text-center btn">HTML</button>
-    <button class="quiz-answer-choice block text-center btn">Rust</button>
-    <button class="quiz-submit block text-center btn btn-blue">Submit</button>
+    <button
+      v-for="(answer, index) in questions[questionIndex].answers"
+      :key="index"
+      class="quiz-answer-choice block text-center btn"
+    >
+      {{ answer }}
+    </button>
+    <button
+      class="quiz-submit block text-center btn btn-blue"
+      @click="submitButtonPressed"
+    >
+      {{ lastQuestion ? 'Submit' : 'Next' }}
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Quiz',
+  props: {
+    questions: {
+      type: Array,
+      default: () => {
+        return [
+          {
+            question:
+              '[SAMPLE QUESTION] Which of these is not a programming language?',
+            answers: ['JavaScript', 'C++', 'HTML', 'Rust'],
+            correct: 2,
+          },
+          {
+            question:
+              '[SAMPLE QUESTION] A 64-bit Intel processor can have, at most, 4 pebibytes of physical memory.',
+            answers: ['True', 'False'],
+            correct: 0,
+          },
+          {
+            question:
+              '[SAMPLE QUESTION] The POSIX function for creating new processes is:',
+            answers: ['pthread_init', 'fork', 'mmap', 'sem_init'],
+            correct: 1,
+          },
+        ]
+      },
+    },
+    minimumScore: {
+      type: Number,
+      default: 0.75,
+    },
+  },
+  data() {
+    return {
+      questionIndex: 0,
+    }
+  },
+  computed: {
+    questionCount() {
+      return this.questions.length
+    },
+    lastQuestion() {
+      return this.questionIndex + 1 === this.questionCount
+    },
+  },
+  methods: {
+    submitButtonPressed() {
+      if (!this.lastQuestion) {
+        this.questionIndex++
+      }
+    },
+  },
 }
 </script>
 
