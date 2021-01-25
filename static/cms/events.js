eba1d5f1e78728c58efc03cfeaae0728bb31873e
Jason Ball <jasonericball@gmail.com>
2021-01-24 20:42:41 -0500

    Add quizzes from the CMS to the DB
    Signed-off-by: Jason Ball <jasonericball@gmail.com>

---
+++ static/cms/events.js
@@ -0,0 +1,19 @@
+/* global CMS, SERVER_URL */
+;(function () {
+  console.log('BluestarCMS Events Loading!')
+  CMS.registerEventListener({
+    name: 'prePublish',
+    handler: ({ entry /*, author */ }) => {
+      // console.log({ author, data: entry.get('data').get('uuid') })
+      fetch(SERVER_URL + '/quiz/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          uuid: entry.get('data').get('uuid'),
+        }),
+      })
+    },
+  })
+})()
