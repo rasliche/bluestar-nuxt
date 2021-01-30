b844aa1c572ba46aac5b138d6d1bb17aabcf38e7
Eric Raslich <rasliche@gmail.com>
2021-01-30 11:56:21 -0500

    Accessing environmental variables

--- netlify-plugin-server-url-swap/index.js
+++ netlify-plugin-server-url-swap/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     onPreBuild: () => {
         console.log('Server URL Swap onPreBuild hook fired')
+        console.log(`Grabbing BASE_URL: ${process.env.BASE_URL}`)
     }
 }
