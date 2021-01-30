2bfbe89983a1497201dd5d9d4e2aaa2576f16b05
Eric Raslich <rasliche@gmail.com>
2021-01-30 15:29:02 -0500

    does the regex work?

--- netlify-plugin-server-url-swap/index.js
+++ netlify-plugin-server-url-swap/index.js
@@ -2,15 +2,19 @@ const fs = require('fs')
 
 module.exports = {
     onPreBuild: () => {
+        const filename = '../static/cms/index.html'
+        const serverUrlRegex = /(?<=SERVER_URL = )*$/g
+        const replacementServerUrl = process.env.BASE_URL
+
         console.log('Server URL Swap onPreBuild hook fired')
         console.log(`Grabbing BASE_URL: ${process.env.BASE_URL}`)
-        fs.readFileSync('../static/cms/index.html', 'utf8', function(err, data) {
+        fs.readFileSync(filename, 'utf8', function(err, data) {
             if (err) {
                 return console.log(err)
             }
-            let result = data.replace(/string to be replaced/g, 'replacement string')
+            let result = data.replace(serverUrlRegex, replacementServerUrl)
 
-            fs.writeFileSync('../static/cms/index.html', result, 'utf8', function(err) {
+            fs.writeFileSync(filename, result, 'utf8', function(err) {
                 if (err) return console.log(err)
             })
         })
