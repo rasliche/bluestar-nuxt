2da7120d4484158a646fa0d5e951695c01afe90e
Eric Raslich <rasliche@gmail.com>
2021-01-30 13:43:25 -0500

    SO solution

--- netlify-plugin-server-url-swap/index.js
+++ netlify-plugin-server-url-swap/index.js
@@ -1,6 +1,18 @@
+const fs = require('fs')
+
 module.exports = {
     onPreBuild: () => {
         console.log('Server URL Swap onPreBuild hook fired')
         console.log(`Grabbing BASE_URL: ${process.env.BASE_URL}`)
+        fs.readFileSync('../static/cms/index.html', 'utf8', function(err, data) {
+            if (err) {
+                return console.log(err)
+            }
+            let result = data.replace(/string to be replaced/g, 'replacement string')
+
+            fs.writeFileSync('../static/cms/index.html', result, 'utf8', function(err) {
+                if (err) return console.log(err)
+            })
+        })
     }
 }
