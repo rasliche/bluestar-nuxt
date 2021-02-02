6549768c963d121521c2175694a268e292766de7
Eric Raslich <rasliche@gmail.com>
2021-02-02 09:07:14 -0500

    Placeholder form stuff

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -5,11 +5,19 @@
         <SearchSelect v-model="user" label="All Users" :options="userNames" />
       </div>
     </div>
-    <div class="space-y-3">
+    <div class="border-t-4 border-gray-300">
+      <div class="max-w-xl px-8">
+        <span class="font-bold text-sm text-blue-800"
+          >New Business Account</span
+        >
+        <CreateBusinessForm></CreateBusinessForm>
+      </div>
+    </div>
+    <!-- <div class="space-y-3">
       <div v-for="user in users" :key="user.id">
         {{ user }}
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -36,6 +44,12 @@ export default {
       //   message: 'Could not load users at this time.',
       // })
     }
+  },
+  methods: {
+    createShop() {},
+    // searchNames(x, y) {
+    //   return x.map((y) => y.includes(x))
+    // },
   },
   head() {
     return [
