7a95fd854c2831ecf0303e61be1d0a41df65ea72
Eric Raslich <rasliche@gmail.com>
2021-02-02 10:14:02 -0500

    Dump changes

---
+++ components/ContinuingEducationForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form method="post" @submit.prevent="createContinuingEducationRecord()">
+      <BaseInput v-model="newShop.name" label="Name"></BaseInput>
+      <BaseInput v-mode="newShop.access_code" label="Access Code"></BaseInput>
+      <!-- <BaseInput></BaseInput> -->
+      <ButtonPrimary @click.prevent="createContinuingEducationRecord()"
+        >Create</ButtonPrimary
+      >
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newShop: {
+        name: null,
+        access_code: null,
+        programs: null,
+      },
+    }
+  },
+  methods: {
+    createShop() {
+      console.log('Creating a new form...')
+    },
+  },
+}
+</script>
+
+<style></style>
