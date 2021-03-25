09a4a39633475014eb71ac427beef79adb28252b
Eric Raslich <rasliche@gmail.com>
2021-03-25 15:02:49 -0400

    Removed comments

--- server/test/integration/auth.test.js
+++ server/test/integration/auth.test.js
@@ -1,33 +1,13 @@
 const request = require('supertest')
-// const Operator = require('../../models/operator')
-const User = require('../../models/User')
 
 describe('/auth routes', () => {
     let server
-    let authUser
-    let adminUser
+
     beforeEach(async() => {
         server = require('../../index')
-        // // Create a user to generate an auth token
-        // authUser = new User({
-        //     name: 'test',
-        //     email: 'user@user.user',
-        //     password: 'tester'
-        // })
-
-        // adminUser = new User({
-        //     name: 'test admin',
-        //     email: 'admin@admin.admin',
-        //     password: 'tester',
-        //     roles: { admin: true }
-        // })
-
-        // await authUser.save(),
-        // await adminUser.save()
     })
     
     afterEach(async () => {
-        // await User.deleteMany({})
         server.close()
     })
 
@@ -42,7 +22,6 @@ describe('/auth routes', () => {
         it.todo('returns 422 if the credentials are invalid')
         it.todo('returns 400 if the user already exists')
         it.todo('returns 201 if the admin registration is successful')
-
     })
 
     describe('POST /auth/manager', () => {
